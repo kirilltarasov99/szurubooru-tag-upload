010bb0ab48ff1a54e4c8c0c84197bcd237125c45
Kirill Tarasov <k.tarasov@g.nsu.ru>
2026-02-24 19:28:57 +0700

    uploading to booru

--- mass_tag_upload.py
+++ mass_tag_upload.py
@@ -1,15 +1,25 @@
 import asyncio
 import re
+import json
+import base64
+import click
+import os
+import requests
+from typing import Final
 from PicImageSearch import Iqdb
 from pathlib import Path
-import click
+
 
 iqdb = Iqdb()
+iqdb_supported = [".jpg", ".jpeg", ".png", ".gif"]
+wd14_supported = [".jpg", ".jpeg", ".png"]
+animated = [".gif"]
+API_URL: Final = os.environ.get("BOORU_API_URL")
+USERNAME: Final = os.environ.get("BOORU_USERNAME")
+LOGIN_TOKEN: Final = os.environ.get("BOORU_TOKEN")
 
-@click.command()
-@click.option('--path', type=str)
-@click.option('--url', type=str)
-@click.option('--key', type=str)
+def btoa(txt: str):
+    return base64.b64encode(txt.encode()).decode()
 
 async def find_image(filepath, similarity):
     results = await iqdb.search(file=filepath)
@@ -47,6 +57,40 @@ async def tag_interrogate(filepath):
     return tags
 
 
+async def upload(file, tags, safety, source):
+    session = requests.Session()
+    session.headers = {
+        "Accept": "application/json",
+        "Authorization": "Token " + btoa("{USERNAME}:{LOGIN_TOKEN}"),
+    }
+    with open(file, "rb") as uploadfile:
+        temporary_data = session.post(
+            f"{API_URL.rstrip('/')}/uploads", files={"content": uploadfile}
+        ).json()
+        file_token = temporary_data["token"]
+
+        try:
+            post_data = session.post(
+                f"{API_URL}/posts",
+                json={"contentToken": file_token, "safety": safety, "tags": tags, "source": source},
+                headers={"Content-Type": "application/json"},
+            )
+            
+            return post_data.json()
+
+        except requests.exceptions.HTTPError as e:
+            try:
+                error_details = post_data.json()
+                print("Szurubooru API error:", error_details)
+            except json.JSONDecodeError:
+                print("Response body:", post_data.text)
+            raise
+        finally:
+            uploadfile.close()  # clean up file handle
+
+@click.command()
+@click.option('--path', type=str)
+
 def main(**kwargs):
     folder = Path(kwargs['path']).resolve()
     paths_files = [p for p in folder.rglob("*") if p.is_file()]
@@ -54,13 +98,28 @@ def main(**kwargs):
     for image in paths_files:
         relative = image.relative_to(folder)
         folder_tags = list(relative.parent.parts)
+        file_type = image.suffix
 
-        tags, rating, link = asyncio.run(find_image(image, 85))
-        if not tags:
-            tags = asyncio.run(tag_interrogate(image))
-            tags.append('ai_generated')
+        if file_type in iqdb_supported:
+            print("iqdb_check")
+            tags, safety, source_link = asyncio.run(find_image(image, 85))
+            if not tags and file_type in wd14_supported:
+                print("wd14_check")
+                tags = asyncio.run(tag_interrogate(image))
+                tags.append('ai_generated')
+                safety = "sketchy"
+                source_link = None
+            
+
+            tags.extend(folder_tags)
         
-        tags.extend(folder_tags)
+        elif file_type not in iqdb_supported and file_type not in wd14_supported:
+            continue
+
+        else:
+            tags = ["no_autotag"]
+        print("pass")
+        asyncio.run(upload(image, tags, safety, source_link))
 
 
 if __name__ == "__main__":
